f6b91d5a85bf3f64d45478d3c3dae0dd2719e99a
Cyrus <cyrusyong1@gmail.com>
2023-12-11 16:45:58 -0500

    added returnWeek functionality, renamed various functions to avoid name collisions

--- app/lib/date.js
+++ app/lib/date.js
@@ -1,58 +1,87 @@
 export default class DateClass {
-    constructor() {
-        this.date = new Date();
+  constructor() {
+    this.date = new Date();
+    this.days = {
+      0: "Sunday",
+      1: "Monday",
+      2: "Tuesday",
+      3: "Wednesday",
+      4: "Thursday",
+      5: "Friday",
+      6: "Saturday",
+    };
+    this.months = [
+      { month: "January", maxDays: 31 },
+      { month: "February", maxDays: 28 },
+      { month: "March", maxDays: 31 },
+      { month: "April", maxDays: 30 },
+      { month: "May", maxDays: 31 },
+      { month: "June", maxDays: 30 },
+      { month: "July", maxDays: 31 },
+      { month: "August", maxDays: 31 },
+      { month: "September", maxDays: 30 },
+      { month: "October", maxDays: 31 },
+      { month: "November", maxDays: 30 },
+      { month: "December", maxDays: 31 },
+    ];
+  }
+
+  //Returns the month as a String
+  returnMonth() {
+    const monthIndex = this.date.getMonth();
+    return this.months[monthIndex].month;
+  }
+
+  //Date: Nov 1, 2004; will return 1
+  returnDayNum() {
+    return this.date.getDate();
+  }
+
+  //Date: Nov 1, 2004; will return string Monday
+  returnDay() {
+    const day = this.date.getDay();
+    return this.days[day];
+  }
+
+  //Takes a number 0-6 and will return corresponding day. (0 = Sunday ... 6 = Saturday)
+  returnDayFromIndex(date) {
+    return this.days[date];
+  }
+
+  //Returns the year
+  returnYear() {
+    return this.date.getFullYear();
+  }
+
+  //Returns Date
+  returnCurrentDate() {
+    return this.date;
+  }
+
+  //Returns an Array of Objects, Each object represents a day in the week.
+  returnWeek() {
+    let startDate = this.date.getDate() - this.date.getDay();
+    const maxDays = this.months[this.date.getMonth()].maxDays;
+    const week = [];
+    for (let dateIndex = 0; dateIndex < 7; dateIndex++) {
+      if (startDate + dateIndex > maxDays) {
+        startDate = 1 - dateIndex;
+      }
+      week.push({
+        day: this.returnDayFromIndex(dateIndex),
+        date: startDate + dateIndex,
+        isToday: startDate + dateIndex == this.date.getDate(),
+        isTomorrow: week[week.length - 1] && week[week.length - 1].isToday,
+      });
     }
-
-    getMonth() {
-        const month = this.date.getMonth();
-
-        const months = {
-            0: "January",
-            1: "Febuary",
-            2: "March",
-            3: "April",
-            4: "May",
-            5: "June",
-            6: "July",
-            7: "August",
-            8: "September",
-            9: "October",
-            10: "November",
-            11: "December",
-        };
-
-        return months[month];
+    if (
+      week[week.length - 1].isToday &&
+      week[0].date > week[week.length - 1].date
+    ) {
+      week[0].isTomorrow = true;
+    } else {
+      week[0].isTomorrow = false;
     }
-
-    getDayNum() {
-        return this.date.getDate();
-    }
-
-    getDay() {
-        const day = this.date.getDay();
-
-        const days = {
-            0: "Sunday",
-            1: "Monday", 
-            2: "Tuesday",
-            3: "Wednesday",
-            4: "Thursday",
-            5: "Friday",
-            6: "Saturday"
-        }
-
-        return days[day];
-    }
-
-    getYear() {
-        return this.date.getFullYear();
-    }
-
-    getCurrentDate() {
-        return this.date;
-    }
-
-    getWeek() {
-        
-    }
+    return week;
+  }
 }
