70e2a272a51572a00e894fc18fdc827515edd9dc
Cyrus <cyrusyong1@gmail.com>
2023-12-10 19:54:16 -0500

    add date functionality

--- app/app/upcoming/page.jsx
+++ app/app/upcoming/page.jsx
@@ -1,3 +1,14 @@
+'use client'
+import DateClass from "../../lib/date";
+
 export default function Page() {
-    return <p>Upcoming Page</p>
+    const date = new DateClass();
+    let month = date.getMonth()
+    let day = date.getDayNum();
+    let year = date.getYear();
+
+    return(
+        <h1 className="font-extrabold text-xl">{month + " " + day + ", " + year}</h1>
+
+    )
 }
