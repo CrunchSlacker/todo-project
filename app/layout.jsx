a19bf3a77c1f154ee3a4071fe2a06d6f6a634377
Cyrus <cyrusyong1@gmail.com>
2023-12-10 19:54:48 -0500

    use open sans font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
 import "@/app/ui/global.css";
+import { open_sans } from "./ui/fonts";
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className="bg-[#FFF9EB]">{children}</body>
+            <body className={`${open_sans.className}bg-[#FFF9EB]`}>{children}</body>
         </html>
     );
 }
