4f2240c25b9d5dd5109e5acdd316a953eac38c8f
albertying <albertlying@gmail.com>
2023-12-11 00:50:42 -0500

    fix formatting

--- app/lib/date.js
+++ app/lib/date.js
@@ -1,58 +1,56 @@
 export default class DateClass {
-    constructor() {
-        this.date = new Date();
-    }
+  constructor() {
+    this.date = new Date();
+  }
 
-    getMonth() {
-        const month = this.date.getMonth();
+  getMonth() {
+    const month = this.date.getMonth();
 
-        const months = {
-            0: "January",
-            1: "Febuary",
-            2: "March",
-            3: "April",
-            4: "May",
-            5: "June",
-            6: "July",
-            7: "August",
-            8: "September",
-            9: "October",
-            10: "November",
-            11: "December",
-        };
+    const months = {
+      0: "January",
+      1: "Febuary",
+      2: "March",
+      3: "April",
+      4: "May",
+      5: "June",
+      6: "July",
+      7: "August",
+      8: "September",
+      9: "October",
+      10: "November",
+      11: "December",
+    };
 
-        return months[month];
-    }
+    return months[month];
+  }
 
-    getDayNum() {
-        return this.date.getDate();
-    }
+  getDayNum() {
+    return this.date.getDate();
+  }
 
-    getDay() {
-        const day = this.date.getDay();
+  getDay() {
+    const day = this.date.getDay();
 
-        const days = {
-            0: "Sunday",
-            1: "Monday", 
-            2: "Tuesday",
-            3: "Wednesday",
-            4: "Thursday",
-            5: "Friday",
-            6: "Saturday"
-        }
+    const days = {
+      0: "Sunday",
+      1: "Monday",
+      2: "Tuesday",
+      3: "Wednesday",
+      4: "Thursday",
+      5: "Friday",
+      6: "Saturday",
+    };
 
-        return days[day];
-    }
+    return days[day];
+  }
 
-    getYear() {
-        return this.date.getFullYear();
-    }
+  getYear() {
+    return this.date.getFullYear();
+  }
 
-    getCurrentDate() {
-        return this.date;
-    }
+  getCurrentDate() {
+    return this.date;
+  }
 
-    getWeek() {
-        
-    }
+  getWeek() {}
 }
